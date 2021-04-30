b6104ef981471032b62a5dc09a3cff16059572c5
shishamo <bnsgny@gmail.com>
2021-04-30 20:14:45 +0900

    add TaskDao.kt (#2)

---
+++ app/src/main/java/com/example/todoappmvp/data/local/TaskDao.kt
@@ -0,0 +1,32 @@
+package com.example.todoappmvp.data.local
+
+import androidx.room.*
+import com.example.todoappmvp.data.Task
+
+@Dao
+interface TaskDao {
+
+    @Query("SELECT * FROM tasks")
+    fun getTasks(): List<Task>
+
+    @Query("SELECT * FROM tasks WHERE entryid = :taskId")
+    fun getTaskById(taskId: String): Task
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insetTask(task: Task)
+
+    @Update
+    fun updateTask(task: Task): Int
+
+    @Query("UPDATE tasks SET completed = :completed WHERE entryid = :taskId")
+    fun updateCompleted(taskId: String, completed: Boolean)
+
+    @Query("DELETE FROM tasks WHERE entryid = :taskId")
+    fun deleteTaskById(taskId: String)
+
+    @Query("DELETE FROM tasks")
+    fun deleteTasks()
+
+    @Query("DELETE FROM tasks WHERE completed = 1")
+    fun deleteCompletedTasks()
+}
