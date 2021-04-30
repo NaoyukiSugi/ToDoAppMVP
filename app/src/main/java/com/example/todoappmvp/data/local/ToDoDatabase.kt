566cf473a9208a10f68f3c59cdc1abda19f77cc6
Naoyuki Sugi <bnsgny@gmail.com>
2021-04-30 21:00:10 +0900

    add ToDoDatabase

---
+++ app/src/main/java/com/example/todoappmvp/data/local/ToDoDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.todoappmvp.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todoappmvp.data.Task
+
+@Database(entities = [Task::class], version = 1)
+abstract class ToDoDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        const val DATABASE_NAME = "Tasks.db"
+
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getInstance(context: Context): ToDoDatabase =
+            INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    ToDoDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+                    .also { INSTANCE = it }
+            }
+    }
+}
