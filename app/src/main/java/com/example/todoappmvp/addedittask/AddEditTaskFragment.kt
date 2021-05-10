d9f8b3833cb85d7742f3599bc7441ca227112062
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-10 19:02:49 +0900

    add fragment

---
+++ app/src/main/java/com/example/todoappmvp/addedittask/AddEditTaskFragment.kt
@@ -0,0 +1,10 @@
+package com.example.todoappmvp.addedittask
+
+import androidx.fragment.app.Fragment
+
+class AddEditTaskFragment : Fragment() {
+
+    companion object {
+        const val ARGUMENT_EDIT_TASK_ID = "EDIT_TASK_ID"
+    }
+}
